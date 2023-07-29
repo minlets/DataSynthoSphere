26324b279edef1d53eb8a06c9c8041efaf37067a
jallavidya-sagar <127670836+jallavidya-sagar@users.noreply.github.com>
2023-07-29 22:21:54 +0530

    Create interactive_mode.py

---
+++ src/main/interactive_mode.py
@@ -0,0 +1,10 @@
+import json
+import logging
+
+
+class InteractiveMode:
+    def __init__(self):
+        # ... (Initialize necessary variables and data structures)
+
+    def run(self):
+        # ... (Add interactive mode logic to guide users through data generation)
