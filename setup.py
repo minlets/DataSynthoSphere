6ad2b1cb10dcca6a2e494473c446251b49211388
jallavidya-sagar <127670836+jallavidya-sagar@users.noreply.github.com>
2023-07-29 22:47:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
-<setup.py>
+from setuptools import setup, find_packages
+
+setup(
+    name='DataSynthoSphere',
+    version='1.0.0',
+    description='DataSynthoSphere is an innovative and cutting-edge AI-powered Synthetic Data project that revolutionizes how organizations understand and interact with their customer data in the digital era. As businesses increasingly shift to online interactions and face stringent data privacy regulations, the need for privacy-preserving customer insights becomes more critical than ever.',
+    author='Vidya Sagar Jalla',
+    author_email='jallavidya.sagar@icloud.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'data_generator = main:main'
+        ],
+    },
+    install_requires=[
+        'openpyxl',
+        'PyYAML'
+    ],
+)
