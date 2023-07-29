7d343718677baa75673e8a18d7e54422f3858a4e
jallavidya-sagar <127670836+jallavidya-sagar@users.noreply.github.com>
2023-07-29 22:21:21 +0530

    Create data_generator.py

---
+++ src/main/data_generator.py
@@ -0,0 +1,19 @@
+import json
+import logging
+import openpyxl
+from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.drawing.image import Image
+
+# ... (Default configuration data and other constants)
+
+
+class DataGenerator:
+    def __init__(self):
+        # ... (Initialize necessary variables and data structures)
+
+    def generate_data(self, output_file=None, file_format='json'):
+        # ... (The existing generate_data function)
+
+    def validate_generated_data(self, data):
+        #
