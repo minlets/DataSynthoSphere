6964130ab62fd85e8aa5c83a80ed9a3317926564
jallavidya-sagar <127670836+jallavidya-sagar@users.noreply.github.com>
2023-07-29 22:49:48 +0530

    Update interactive_mode.py

--- src/main/interactive_mode.py
+++ src/main/interactive_mode.py
@@ -4,7 +4,7 @@ import logging
 
 class InteractiveMode:
     def __init__(self):
-        # ... (Initialize necessary variables and data structures)
+        print("# ... (Initialize necessary variables and data structures)")
 
     def run(self):
-        # ... (Add interactive mode logic to guide users through data generation)
+        print("# ... (Add interactive mode logic to guide users through data generation)")
