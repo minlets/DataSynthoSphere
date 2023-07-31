fb7cc599f98a43581c560ee2276c2b1a05f0ba45
jallavidya-sagar <127670836+jallavidya-sagar@users.noreply.github.com>
2023-07-31 21:43:58 +0530

    Updated and renamed DataGenerator.py to data_generate.py v1

---
+++ src/main/data_generate.py
@@ -0,0 +1,7 @@
+import json
+
+class DataGenerator:
+    @staticmethod
+    def generate_data(config):
+        # Your code for generating synthetic data using the given configuration goes here
+        return {"result": "Generated data"}
