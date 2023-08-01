05c5a31beb7b328cd8da788be3808a07acf70a9b
jallavidya-sagar <127670836+jallavidya-sagar@users.noreply.github.com>
2023-08-01 15:53:26 +0000

    done

--- src/main/DataGenerator.py
+++ src/main/DataGenerator.py
@@ -1,7 +1,6 @@
 import json
 
 class DataGenerator:
-    @staticmethod
     def generate_data(config):
         # Your code for generating synthetic data using the given configuration goes here
         return {"result": "Generated data"}
